54836feecc4da892907ca74dd0514f8f7fc4a952
Mohit Sati <146699961+SuperexMack@users.noreply.github.com>
2023-10-31 22:34:20 +0530

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int i = 0;
+    int j = 1;
+    int sum = 0;
+    int n = 7;
+    int k = 0;
+
+    // 0 0 1 2 3 5 8
+
+    while (k < n - 1)
+    {
+
+        sum = j + i;
+
+        i = j;
+        j = sum;
+        k++;
+    }
+
+    cout << sum;
+}
